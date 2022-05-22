981e5e6933e2ce59588f9c9bc753645fce022045
Jeton <90478926+JetonShabani@users.noreply.github.com>
2022-05-22 14:10:52 +0200

    initialized app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import IBAN from "iban";
+
+import "./App.css";
+
+function isValid(iban) {
+  if (iban.substring(0, 2) !== "LI") return false;
+  return IBAN.isValid(iban);
+}
 
 function App() {
+  const [input, setInput] = useState("DE11520513735120710131");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IBAN</h1>
+      <p>Start typing: </p>
+      <p>Example Liechtenstein: LI21088100002324013AA</p>
+      <input
+        onChange={e => setInput(e.target.value)}
+        placeholder="Digit a valid IBAN"
+      />
+      <p>Your Input: {input}</p>
+      <p>Is Valid IBAN? {IBAN.isValid(input).toString()}</p>
+      <p>Is Valid Liechtenstein IBAN: {isValid(input).toString()}</p>
     </div>
   );
 }
-
+ 
 export default App;
